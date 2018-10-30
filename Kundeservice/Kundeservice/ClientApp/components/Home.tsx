51793227cb21920520ee3775a462685fbeb0aef0
Carlo <s315613@stud.hioa.no>
2018-10-30 21:02:32 +0100

    init

--- Kundeservice/Kundeservice/ClientApp/components/Home.tsx
+++ Kundeservice/Kundeservice/ClientApp/components/Home.tsx
@@ -2,9 +2,51 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
-    public render() {
-        return <div>
-            <h1>Hello, world!</h1>
-        </div>;
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+        return null;
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Customer Support!</h1>
+                <form>
+                    <FormGroup
+                        controlId="formBasicText"
+                        validationState={this.getValidationState()}
+                    >
+                        <ControlLabel>Your Topic</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.value}
+                            placeholder="Enter your topic"
+                            onChange={this.handleChange}
+                        />
+                        <FormControl.Feedback />
+                        <HelpBlock>Sum text</HelpBlock>
+                    </FormGroup>
+                </form>
+        </div>
+        );
     }
 }
+render(<FormExample/>);
+
