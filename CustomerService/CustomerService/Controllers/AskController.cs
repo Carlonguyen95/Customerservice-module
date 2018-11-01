c70f7bbfe3de88aa1ad2b3668c9b5c72f2b68ef2
Carlo <s315613@stud.hioa.no>
2018-11-01 13:27:03 +0100

    installed EF Core

---
+++ CustomerService/CustomerService/Controllers/AskController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ask")]
+    public class AskController : Controller
+    {
+
+    }
+}
---
+++ CustomerService/CustomerService/Models/DB/DBModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerService.Models.DB
+{
+    public class DBModel : DbContext
+    {
+        public DBModel(DbContextOptions<DBModel> options) : base(options) { }
+
+        public DbSet<CustomerAsk> CustomerAsk { get; set; }  
+    }
+
+    public class CustomerAsk
+    {
+        public int CustomerID { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerQuestion { get; set; }
+    }
+}
